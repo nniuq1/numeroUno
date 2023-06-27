9a8cf38f5e342177cd098d7d5b0e04c14ce8d0f2
reidthegreatandpowerfulandterible <reid.monahan@gmail.com>
2023-06-27 14:39:06 -0700

    added sword animations

--- My project (2)/Assets/Scripts/meleeHeld.cs
+++ My project (2)/Assets/Scripts/meleeHeld.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class meleeHeld : MonoBehaviour
 {
+    public Animator animator;
+    public GameObject explosion;
     public inventory Inventory;
     public LayerMask meleeMask;
 
@@ -12,21 +14,24 @@ public class meleeHeld : MonoBehaviour
         if (transform.parent.parent.GetChild(1).GetComponent<Camera>().ScreenToWorldPoint(Input.mousePosition).x > transform.parent.position.x)
         {
             transform.localScale = Inventory.itemClasses[(int)Inventory.itemSelected].weaponSize;
-            transform.rotation = Quaternion.Euler(0, 0, -90);
+            animator.SetBool("right", true);
             transform.position = new Vector2(transform.parent.position.x + transform.localScale.y / 2, transform.parent.position.y);
         }
         else
         {
             transform.localScale = Inventory.itemClasses[(int)Inventory.itemSelected].weaponSize;
-            transform.rotation = Quaternion.Euler(0, 0, 90);
+            animator.SetBool("right", false);
             transform.position = new Vector2(transform.parent.position.x - transform.localScale.y / 2, transform.parent.position.y);
         }
 
         if (Input.GetMouseButtonDown(0))
         {
+            animator.SetBool("attacking", true);
+            StartCoroutine(stopAttacking());
+
             Collider2D[] attackBox;
 
-            if (Quaternion.ToEulerAngles(transform.rotation).z == -Mathf.PI / 2)
+            if (animator.GetBool("right") == true)
             {
                 attackBox = Physics2D.OverlapBoxAll(new Vector2(transform.parent.position.x + (transform.localScale.x + 1.55f) / 2, transform.parent.position.y), Inventory.itemClasses[(int)Inventory.itemSelected].MeleeAtackArea, 0, meleeMask);
             }
@@ -37,8 +42,25 @@ public class meleeHeld : MonoBehaviour
 
             for (int i = 0; i < attackBox.Length; i++)
             {
+                if (Inventory.itemClasses[(int)Inventory.itemSelected].explodes)
+                {
+                    if (animator.GetBool("right") == true)
+                    {
+                        Instantiate(explosion, attackBox[i].transform.position, Quaternion.Euler(0, 90, 0));
+                    }
+                    else
+                    {
+                        Instantiate(explosion, attackBox[i].transform.position, Quaternion.Euler(0, -90, 0));
+                    }
+                }
                 Destroy(attackBox[i].gameObject);
             }
         }
+    }
+
+    IEnumerator stopAttacking()
+    {
+        yield return new WaitForSeconds(0.1f);
+        animator.SetBool("attacking", false);
     }
 }
