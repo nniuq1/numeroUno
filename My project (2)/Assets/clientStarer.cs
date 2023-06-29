59151dfc6ac31b90fa43ae7928b5c3a3b20857f8
reidthegreatandpowerfulandterible <reid.monahan@gmail.com>
2023-06-29 13:41:10 -0700

    added temporary settings camera

--- My project (2)/Assets/clientStarer.cs
+++ My project (2)/Assets/clientStarer.cs
@@ -6,16 +6,19 @@ using UnityEngine;
 public class clientStarer : MonoBehaviour
 {
     public NetworkManager bees;
+    public Camera tempCamera;
 
     public void ReleasetheBees()
     {
         bees.StartClient();
+        Destroy(tempCamera.gameObject);
         transform.GetChild(0).gameObject.SetActive(false);
         transform.GetChild(1).gameObject.SetActive(false);
     }
     public void formTheHive()
     {
         bees.StartHost();
+        Destroy(tempCamera.gameObject);
         transform.GetChild(0).gameObject.SetActive(false);
         transform.GetChild(1).gameObject.SetActive(false);
     }
