18c35c8fc66705d672e9f95a15b74da907195413
reidthegreatandpowerfulandterible <reid.monahan@gmail.com>
2023-06-29 11:35:33 -0700

    start of playerhealth transfering

--- My project (2)/Assets/Scripts/playerHealth.cs
+++ My project (2)/Assets/Scripts/playerHealth.cs
@@ -7,6 +7,7 @@ public class playerHealth : NetworkBehaviour
 {
     public NetworkVariable<float> _netHealth = new NetworkVariable<float>(writePerm: NetworkVariableWritePermission.Owner);
     public float startHealth = 10;
+    public float health;
 
     private void Start()
     {
@@ -18,7 +19,6 @@ public class playerHealth : NetworkBehaviour
 
     public void TakeDamage(float damage)
     {
-        print("owie wowie");
         _netHealth.Value -= damage;
     }
 
