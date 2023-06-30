1b2aa9dd5d48b066d60ed5bb623281af2ca80a39
reidthegreatandpowerfulandterible <reid.monahan@gmail.com>
2023-06-30 13:13:40 -0700

    added main menu

---
+++ My project (2)/Assets/Scripts/mainMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+    public void Starting()
+    {
+        SceneManager.LoadScene("testing");
+    }
+
+    public void Settings()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
