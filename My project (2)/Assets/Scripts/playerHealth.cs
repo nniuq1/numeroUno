1683ddc0f25d719b653431161588f55afba4f1f9
reidthegreatandpowerfulandterible <reid.monahan@gmail.com>
2023-07-03 10:46:42 -0700

    8oqiuflehkrjaro;sl.kzjgvdns

--- My project (2)/Assets/Scripts/playerHealth.cs
+++ My project (2)/Assets/Scripts/playerHealth.cs
@@ -42,6 +42,11 @@ public class playerHealth : NetworkBehaviour
         if (IsOwner)
         {
             Object.FindObjectOfType<Canvas>().transform.GetChild(3).GetChild(1).transform.localScale = new Vector2(_netHealth.Value / startHealth, 1);
+            if (_netHealth.Value <= 0)
+            {
+                health = startHealth;
+                transform.position = Vector3.zero;
+            }
         }
     }
 
