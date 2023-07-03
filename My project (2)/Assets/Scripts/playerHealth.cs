7337ec1c3ba27802dbe7f80b9f67c2bcc794d747
reidthegreatandpowerfulandterible <reid.monahan@gmail.com>
2023-07-03 11:00:17 -0700

    Update playerHealth.cs

--- My project (2)/Assets/Scripts/playerHealth.cs
+++ My project (2)/Assets/Scripts/playerHealth.cs
@@ -43,7 +43,7 @@ public class playerHealth : NetworkBehaviour
             Object.FindObjectOfType<Canvas>().transform.GetChild(3).GetChild(1).transform.localScale = new Vector2(_netHealth.Value / startHealth, 1);
             if (_netHealth.Value <= 0)
             {
-                health = startHealth;
+                TakeDamage(-startHealth);
                 TestServerRpc();
                 transform.position = Vector3.zero;
             }
