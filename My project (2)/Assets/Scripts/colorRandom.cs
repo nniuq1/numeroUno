8b8caba3b149f25a00cec42728758ccf463caac9
reidthegreatandpowerfulandterible <reid.monahan@gmail.com>
2023-07-03 09:34:39 -0700

    welfukhsnpow;eilghesvnil fjkdz,xbmnvmc./w dSx

--- My project (2)/Assets/Scripts/colorRandom.cs
+++ My project (2)/Assets/Scripts/colorRandom.cs
@@ -6,7 +6,7 @@ using Unity.Netcode;
 public class colorRandom : NetworkBehaviour
 {
     public NetworkVariable<Color> _netColor = new NetworkVariable<Color>(writePerm: NetworkVariableWritePermission.Owner);
-    private bool t = false;
+    private int t = 2;
 
     void Start()
     {
@@ -15,20 +15,13 @@ public class colorRandom : NetworkBehaviour
             _netColor.Value = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
             transform.GetComponent<SpriteRenderer>().color = _netColor.Value;
         }
-        StartCoroutine(waitchangeColour());
     }
     void Update()
     {
-        if (!IsOwner && t)
+        if (!IsOwner && t > 0)
         {
             transform.GetComponent<SpriteRenderer>().color = _netColor.Value;
-            t = false;
+            t--;
         }
-    }
-
-    IEnumerator waitchangeColour()
-    {
-        yield return new WaitForSeconds(0.1f);
-        t = true;
     }
 }
