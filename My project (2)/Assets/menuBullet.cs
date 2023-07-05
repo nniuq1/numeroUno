abd6f940b5250aaa72f3391adf44b9935254ce86
reidthegreatandpowerfulandterible <reid.monahan@gmail.com>
2023-07-05 10:18:49 -0700

    idk

--- My project (2)/Assets/menuBullet.cs
+++ My project (2)/Assets/menuBullet.cs
@@ -1,11 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class menuBullet : MonoBehaviour
 {
     private void Update()
     {
         transform.position = new Vector2(transform.position.x + Mathf.Cos(Quaternion.ToEulerAngles(transform.rotation).z) * Time.deltaTime * 15, transform.position.y + Mathf.Sin(Quaternion.ToEulerAngles(transform.rotation).z) * Time.deltaTime * 15);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("startgame"))
+        {
+            SceneManager.LoadScene("testing");
+        }
+        if (collision.CompareTag("settings"))
+        {
+
+        }
+        if (collision.CompareTag("quit"))
+        {
+            Application.Quit();
+        }
     }
 }
