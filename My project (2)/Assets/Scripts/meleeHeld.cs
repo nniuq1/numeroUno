f86d640f25ea555d349e1957f75807f6ca6c4f25
reidthegreatandpowerfulandterible <reid.monahan@gmail.com>
2023-07-11 14:09:36 -0700

    Update meleeHeld.cs

--- My project (2)/Assets/Scripts/meleeHeld.cs
+++ My project (2)/Assets/Scripts/meleeHeld.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
-public class meleeHeld : MonoBehaviour
+public class meleeHeld : NetworkBehaviour
 {
     public GameObject objectBreaking;
     bool canHit = true;
@@ -29,49 +30,57 @@ public class meleeHeld : MonoBehaviour
             transform.GetComponent<SpriteRenderer>().flipX = true;
         }
 
-        if (Input.GetMouseButtonDown(0) && canHit && !Inventory.itemClasses[(int)Inventory.itemSelected.Value].canHoldDown || Input.GetMouseButton(0) && canHit && Inventory.itemClasses[(int)Inventory.itemSelected.Value].canHoldDown)
+        if (IsServer)
         {
-            StartCoroutine(timeBetweenHits());
-            animator.speed = 1f / Inventory.itemClasses[(int)Inventory.itemSelected.Value].attackSpeed;
-            animator.SetBool("attacking", true);
-            StartCoroutine(stopAttacking());
-
-            Collider2D[] attackBox;
-
-            if (animator.GetBool("right") == true)
+            if (Input.GetMouseButtonDown(0) && canHit && !Inventory.itemClasses[(int)Inventory.itemSelected.Value].canHoldDown || Input.GetMouseButton(0) && canHit && Inventory.itemClasses[(int)Inventory.itemSelected.Value].canHoldDown)
             {
-                attackBox = Physics2D.OverlapBoxAll(new Vector2(transform.parent.position.x + (transform.localScale.x + 1.55f) / 2, transform.parent.position.y), Inventory.itemClasses[(int)Inventory.itemSelected.Value].MeleeAtackArea, 0, meleeMask);
-            }
-            else
-            {
-                attackBox = Physics2D.OverlapBoxAll(new Vector2(transform.parent.position.x - (transform.localScale.x + 1.55f) / 2, transform.parent.position.y), Inventory.itemClasses[(int)Inventory.itemSelected.Value].MeleeAtackArea, 0, meleeMask);
-            }
+                animationClientRPC(NetworkManager.Singleton.LocalClientId);
+                StartCoroutine(timeBetweenHits());
+                animator.speed = 1f / Inventory.itemClasses[(int)Inventory.itemSelected.Value].attackSpeed;
+                animator.SetBool("attacking", true);
+                StartCoroutine(stopAttacking());
 
-            for (int i = 0; i < attackBox.Length; i++)
-            {
-                if (Inventory.itemClasses[(int)Inventory.itemSelected.Value].explodes)
+                Collider2D[] attackBox;
+
+                if (animator.GetBool("right") == true)
                 {
-                    if (animator.GetBool("right") == true)
+                    attackBox = Physics2D.OverlapBoxAll(new Vector2(transform.parent.position.x + (transform.localScale.x + 1.55f) / 2, transform.parent.position.y), Inventory.itemClasses[(int)Inventory.itemSelected.Value].MeleeAtackArea, 0, meleeMask);
+                }
+                else
+                {
+                    attackBox = Physics2D.OverlapBoxAll(new Vector2(transform.parent.position.x - (transform.localScale.x + 1.55f) / 2, transform.parent.position.y), Inventory.itemClasses[(int)Inventory.itemSelected.Value].MeleeAtackArea, 0, meleeMask);
+                }
+
+                for (int i = 0; i < attackBox.Length; i++)
+                {
+                    if (Inventory.itemClasses[(int)Inventory.itemSelected.Value].explodes)
                     {
-                        GameObject explodesing = Instantiate(explosion, new Vector3(transform.position.x + 1.75f, transform.position.y, 0), Quaternion.Euler(0, 0, 0));
-                        explodesing.GetComponent<explosionDamage>().player = transform.parent.parent.gameObject;
+                        if (animator.GetBool("right") == true)
+                        {
+                            GameObject explodesing = Instantiate(explosion, new Vector3(transform.position.x + 1.75f, transform.position.y, 0), Quaternion.Euler(0, 0, 0));
+                            explodesing.GetComponent<explosionDamage>().player = transform.parent.parent.gameObject;
+                        }
+                        else
+                        {
+                            GameObject explodesing = Instantiate(explosion, new Vector3(transform.position.x - 1.75f, transform.position.y, 0), Quaternion.Euler(0, 0, 0));
+                            explodesing.GetComponent<explosionDamage>().player = transform.parent.parent.gameObject;
+                        }
                     }
-                    else
+                    if (!attackBox[i].CompareTag("Player"))
                     {
-                        GameObject explodesing = Instantiate(explosion, new Vector3(transform.position.x - 1.75f, transform.position.y, 0), Quaternion.Euler(0, 0, 0));
-                        explodesing.GetComponent<explosionDamage>().player = transform.parent.parent.gameObject;
+                        Instantiate(objectBreaking, attackBox[i].transform.position, attackBox[i].transform.rotation);
+                        Destroy(attackBox[i].gameObject);
                     }
-                }
-                if (!attackBox[i].CompareTag("Player"))
-                {
-                    Instantiate(objectBreaking, attackBox[i].transform.position, attackBox[i].transform.rotation);
-                    Destroy(attackBox[i].gameObject);
-                }
-                else if (attackBox[i].gameObject != transform.parent.parent.gameObject)
-                {
-                    attackBox[i].GetComponent<playerHealth>().TakeDamage(Inventory.itemClasses[(int)Inventory.itemSelected.Value].MeleeDamage);
+                    else if (attackBox[i].gameObject != transform.parent.parent.gameObject)
+                    {
+                        attackBox[i].GetComponent<playerHealth>().TakeDamage(Inventory.itemClasses[(int)Inventory.itemSelected.Value].MeleeDamage);
+                    }
                 }
             }
+        }
+        else
+        {
+            animationServerRPC(NetworkManager.Singleton.LocalClientId);
         }
     }
 
@@ -86,5 +95,61 @@ public class meleeHeld : MonoBehaviour
     {
         yield return new WaitForSeconds(0.01f);
         animator.SetBool("attacking", false);
+    }
+
+    [ClientRpc]
+    void animationClientRPC(ulong player)
+    {
+
+    }
+
+    [ServerRpc]
+    void animationServerRPC(ulong player)
+    {
+        if (Input.GetMouseButtonDown(0) && canHit && !NetworkManager.Singleton.ConnectedClients[player].PlayerObject.GetComponent<inventory>().itemClasses[(int)NetworkManager.Singleton.ConnectedClients[player].PlayerObject.GetComponent<inventory>().itemSelected.Value].canHoldDown || Input.GetMouseButton(0) && canHit && NetworkManager.Singleton.ConnectedClients[player].PlayerObject.GetComponent<inventory>().itemClasses[(int)NetworkManager.Singleton.ConnectedClients[player].PlayerObject.GetComponent<inventory>().itemSelected.Value].canHoldDown)
+        {
+            animationClientRPC(NetworkManager.Singleton.LocalClientId);
+            StartCoroutine(timeBetweenHits());
+            animator.speed = 1f / NetworkManager.Singleton.ConnectedClients[player].PlayerObject.GetComponent<inventory>().itemClasses[(int)NetworkManager.Singleton.ConnectedClients[player].PlayerObject.GetComponent<inventory>().itemSelected.Value].attackSpeed;
+            animator.SetBool("attacking", true);
+            StartCoroutine(stopAttacking());
+
+            Collider2D[] attackBox;
+
+            if (animator.GetBool("right") == true)
+            {
+                attackBox = Physics2D.OverlapBoxAll(new Vector2(NetworkManager.Singleton.ConnectedClients[player].PlayerObject.transform.parent.position.x + (NetworkManager.Singleton.ConnectedClients[player].PlayerObject.transform.localScale.x + 1.55f) / 2, NetworkManager.Singleton.ConnectedClients[player].PlayerObject.transform.parent.position.y), NetworkManager.Singleton.ConnectedClients[player].PlayerObject.GetComponent<inventory>().itemClasses[(int)NetworkManager.Singleton.ConnectedClients[player].PlayerObject.GetComponent<inventory>().itemSelected.Value].MeleeAtackArea, 0, meleeMask);
+            }
+            else
+            {
+                attackBox = Physics2D.OverlapBoxAll(new Vector2(NetworkManager.Singleton.ConnectedClients[player].PlayerObject.transform.parent.position.x - (NetworkManager.Singleton.ConnectedClients[player].PlayerObject.transform.localScale.x + 1.55f) / 2, NetworkManager.Singleton.ConnectedClients[player].PlayerObject.transform.parent.position.y), NetworkManager.Singleton.ConnectedClients[player].PlayerObject.GetComponent<inventory>().itemClasses[(int)NetworkManager.Singleton.ConnectedClients[player].PlayerObject.GetComponent<inventory>().itemSelected.Value].MeleeAtackArea, 0, meleeMask);
+            }
+
+            for (int i = 0; i < attackBox.Length; i++)
+            {
+                if (NetworkManager.Singleton.ConnectedClients[player].PlayerObject.GetComponent<inventory>().itemClasses[(int)NetworkManager.Singleton.ConnectedClients[player].PlayerObject.GetComponent<inventory>().itemSelected.Value].explodes)
+                {
+                    if (animator.GetBool("right") == true)
+                    {
+                        GameObject explodesing = Instantiate(explosion, new Vector3(NetworkManager.Singleton.ConnectedClients[player].PlayerObject.transform.position.x + 1.75f, NetworkManager.Singleton.ConnectedClients[player].PlayerObject.transform.position.y, 0), Quaternion.Euler(0, 0, 0));
+                        explodesing.GetComponent<explosionDamage>().player = NetworkManager.Singleton.ConnectedClients[player].PlayerObject.transform.parent.parent.gameObject;
+                    }
+                    else
+                    {
+                        GameObject explodesing = Instantiate(explosion, new Vector3(NetworkManager.Singleton.ConnectedClients[player].PlayerObject.transform.position.x - 1.75f, NetworkManager.Singleton.ConnectedClients[player].PlayerObject.transform.position.y, 0), Quaternion.Euler(0, 0, 0));
+                        explodesing.GetComponent<explosionDamage>().player = NetworkManager.Singleton.ConnectedClients[player].PlayerObject.transform.parent.parent.gameObject;
+                    }
+                }
+                if (!attackBox[i].CompareTag("Player"))
+                {
+                    Instantiate(objectBreaking, attackBox[i].transform.position, attackBox[i].transform.rotation);
+                    Destroy(attackBox[i].gameObject);
+                }
+                else if (attackBox[i].gameObject != NetworkManager.Singleton.ConnectedClients[player].PlayerObject.transform.parent.parent.gameObject)
+                {
+                    attackBox[i].GetComponent<playerHealth>().TakeDamage(NetworkManager.Singleton.ConnectedClients[player].PlayerObject.GetComponent<inventory>().itemClasses[(int)NetworkManager.Singleton.ConnectedClients[player].PlayerObject.GetComponent<inventory>().itemSelected.Value].MeleeDamage);
+                }
+            }
+        }
     }
 }
