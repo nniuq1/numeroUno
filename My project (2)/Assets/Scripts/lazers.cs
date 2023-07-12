dd20cffe92a5330fadb5adccd534aee76ef1a5da
reidthegreatandpowerfulandterible <reid.monahan@gmail.com>
2023-07-12 13:45:02 -0700

    laser

--- My project (2)/Assets/Scripts/lazers.cs
+++ My project (2)/Assets/Scripts/lazers.cs
@@ -5,6 +5,7 @@ using Unity.Netcode;
 
 public class lazers : NetworkBehaviour
 {
+    [SerializeField] public LineRenderer line;
     public inventory Inventory;
     public GameObject projectile;
     public NetworkVariable<float> _rotations = new NetworkVariable<float>(writePerm: NetworkVariableWritePermission.Owner);
@@ -36,15 +37,22 @@ public class lazers : NetworkBehaviour
         if (shooting.Value)
         {
             //draw raycast
+            RaycastHit2D[] hit = Physics2D.RaycastAll(transform.position, new Vector2(Mathf.Cos(Quaternion.ToEulerAngles(transform.rotation).z), Mathf.Sin(Quaternion.ToEulerAngles(transform.rotation).z)));
+            line.enabled = false;
+            line.SetPosition(0, transform.position);
+            line.SetPosition(0, hit[1].point);
             if (IsServer)
             {
                 //RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(Mathf.Cos(Quaternion.ToEulerAngles(transform.rotation).z), Mathf.Sin(Quaternion.ToEulerAngles(transform.rotation).z)));
-                RaycastHit2D[] hit = Physics2D.RaycastAll(transform.position, new Vector2(Mathf.Cos(Quaternion.ToEulerAngles(transform.rotation).z), Mathf.Sin(Quaternion.ToEulerAngles(transform.rotation).z)));
                 if (hit[1].transform != null && hit[1].transform.GetComponent<playerHealth>() != null)
                 {
                     hit[1].transform.GetComponent<playerHealth>().TakeDamage(3 * Time.deltaTime);
                 }
             }
+        }
+        else
+        {
+            line.enabled = false;
         }
     }
 }
