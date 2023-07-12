fa87c68d987120324343aa509de1dd9ff3231e40
reidthegreatandpowerfulandterible <reid.monahan@gmail.com>
2023-07-12 10:59:47 -0700

    Update wandProjectile.cs

--- My project (2)/Assets/wandProjectile.cs
+++ My project (2)/Assets/wandProjectile.cs
@@ -51,8 +51,7 @@ public class wandProjectile : NetworkBehaviour
                 if (bounces > 0)
                 {
                     bounces--;
-                    int random = (Random.Range(0, 1) * 2) - 1;
-                    print(random);
+                    int random = (Random.Range(0, 2) * 2) - 1;
                     transform.GetComponent<Rigidbody2D>().velocity = new Vector2(item.projectileSpeed * Mathf.Cos(Mathf.Atan2(collision.transform.position.x - transform.position.x, collision.transform.position.y - transform.position.y) + random * Mathf.PI), item.projectileSpeed * Mathf.Sin(Mathf.Atan2(collision.transform.position.x - transform.position.x, collision.transform.position.y - transform.position.y) + random * Mathf.PI));
                 }
                 else
