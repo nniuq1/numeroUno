f65af1a27d299eaa4f2ebf7ff86814fbb7b5ae65
reidthegreatandpowerfulandterible <reid.monahan@gmail.com>
2023-07-13 13:32:17 -0700

    Update playerHealth.cs

--- My project (2)/Assets/Scripts/playerHealth.cs
+++ My project (2)/Assets/Scripts/playerHealth.cs
@@ -90,6 +90,7 @@ public class playerHealth : NetworkBehaviour
     {
         print("I am served");
         GameObject ham = Instantiate(hamburguesaItem, pos, Quaternion.Euler(0, 0, 0));
+        print(ham.transform.position);
         ham.GetComponent<NetworkObject>().Spawn();
         for (int i = 0; i < NetworkManager.Singleton.ConnectedClients[player].PlayerObject.transform.GetComponent<inventory>().itemClasses.Count; i++)
         {
