188754b4845c97ac7dfe28c23fdc0586b8654178
reidthegreatandpowerfulandterible <reid.monahan@gmail.com>
2023-07-18 14:12:52 -0700

    Update catScript.cs

--- My project (2)/Assets/catScript.cs
+++ My project (2)/Assets/catScript.cs
@@ -24,8 +24,6 @@ public class catScript : NetworkBehaviour
 
     private void Update()
     {
-        if (IsOwner)
-        {
             if (IsServer)
             {
                 float up;
@@ -65,7 +63,6 @@ public class catScript : NetworkBehaviour
             {
                 transform.position = _netPos.Value;
             }
-        }
     }
 
     IEnumerator destroy()
