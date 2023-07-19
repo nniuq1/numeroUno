6b7b3cb743a47ee831308d07a32488ccf9605cc9
reidthegreatandpowerfulandterible <reid.monahan@gmail.com>
2023-07-19 13:38:37 -0700

    Update pointingScript.cs

--- My project (2)/Assets/Scripts/pointingScript.cs
+++ My project (2)/Assets/Scripts/pointingScript.cs
@@ -1,58 +1,85 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
-public class pointingScript : MonoBehaviour
+public class pointingScript : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Vector2 targetPos;
+    public itemClass Hamburguesa;
 
-    // Update is called once per frame
     void Update()
     {
-        Vector2 pos = transform.parent.GetChild(1).GetComponent<Camera>().WorldToViewportPoint(Vector2.zero);
-        if ((pos.x<0 || pos.x > 1) || (pos.y<0 || pos.y>1))
+        if (IsOwner)
         {
-            transform.GetChild(0).gameObject.SetActive(true);
-            Vector3 dir = transform.parent.GetChild(1).GetComponent<Camera>().WorldToViewportPoint(Vector2.zero) - transform.position;
-            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-            transform.GetChild(0).transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-            Vector2 arrow = pos;
-            if (arrow.x < .1)
+            bool findInChar = false;
+            for (int i = 0; i < Object.FindObjectsOfType<inventory>().Length; i++)
             {
-                double d = (arrow.x - .5) / 0.4;
-                arrow.x = .1f;
-                arrow.y = (float)((arrow.y - 0.5) / (-d) + .5);
+                for (int h = 0; h < Object.FindObjectsOfType<inventory>()[i].itemClasses.Count; i++)
+                {
+                    if (Object.FindObjectsOfType<inventory>()[i].itemClasses[h] == Hamburguesa)
+                    {
+                        targetPos = transform.parent.GetChild(1).GetComponent<Camera>().WorldToViewportPoint(Object.FindObjectsOfType<inventory>()[i].transform.position);
+                        findInChar = true;
+                    }
+                }
             }
-            else if (arrow.x > .9)
+            if (!findInChar)
             {
-                double d = (arrow.x - 0.5)/0.4;
-                arrow.x = .9f;
-                arrow.y = (float)((arrow.y - 0.5) / d + .5);
+                targetPos = new Vector2(1000, 1000);
+                for (int i = 0; i < Object.FindObjectsOfType<itemScript>().Length; i++)
+                {
+                    if (Object.FindObjectsOfType<itemScript>()[i].itemclass == Hamburguesa)
+                    {
+                        targetPos = transform.parent.GetChild(1).GetComponent<Camera>().WorldToViewportPoint(Object.FindObjectsOfType<itemScript>()[i].transform.position);
+                    }
+                }
             }
-            if (arrow.y < .1)
+
+            //Vector2 pos = transform.parent.GetChild(1).GetComponent<Camera>().WorldToViewportPoint(Vector2.zero);
+            if (targetPos != new Vector2(1000, 1000))
             {
-                double d = (arrow.y - 0.5) / 0.4;
-                arrow.y = .1f;
-                arrow.x = (float)((arrow.x - 0.5) / (-d) + .5);
+                if ((targetPos.x < 0 || targetPos.x > 1) || (targetPos.y < 0 || targetPos.y > 1))
+                {
+                    transform.GetChild(0).gameObject.SetActive(true);
+                    Vector3 dir = transform.parent.GetChild(1).GetComponent<Camera>().WorldToViewportPoint(Vector2.zero) - transform.position;
+                    float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+                    transform.GetChild(0).transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+                    Vector2 arrow = targetPos;
+                    if (arrow.x < .1)
+                    {
+                        double d = (arrow.x - .5) / 0.4;
+                        arrow.x = .1f;
+                        arrow.y = (float)((arrow.y - 0.5) / (-d) + .5);
+                    }
+                    else if (arrow.x > .9)
+                    {
+                        double d = (arrow.x - 0.5) / 0.4;
+                        arrow.x = .9f;
+                        arrow.y = (float)((arrow.y - 0.5) / d + .5);
+                    }
+                    if (arrow.y < .1)
+                    {
+                        double d = (arrow.y - 0.5) / 0.4;
+                        arrow.y = .1f;
+                        arrow.x = (float)((arrow.x - 0.5) / (-d) + .5);
+                    }
+                    else if (arrow.y > .9)
+                    {
+                        double d = (arrow.y - 0.5) / 0.4;
+                        arrow.y = .9f;
+                        arrow.x = (float)((arrow.x - 0.5) / d + .5);
+                    }
+                    arrow.y = arrow.y * transform.parent.GetChild(1).GetComponent<Camera>().pixelHeight;
+                    arrow.x = arrow.x * transform.parent.GetChild(1).GetComponent<Camera>().pixelWidth;
+                    Vector2 p = transform.parent.GetChild(1).GetComponent<Camera>().ScreenToWorldPoint(arrow);
+                    transform.GetChild(0).transform.position = p;
+                }
+                else
+                {
+                    transform.GetChild(0).gameObject.SetActive(false);
+                }
             }
-            else if (arrow.y > .9)
-            {
-                double d = (arrow.y - 0.5) / 0.4;
-                arrow.y = .9f;
-                arrow.x = (float)((arrow.x - 0.5)/ d +.5);
-            }
-            arrow.y = arrow.y * transform.parent.GetChild(1).GetComponent<Camera>().pixelHeight;
-            arrow.x = arrow.x * transform.parent.GetChild(1).GetComponent<Camera>().pixelWidth;
-            Vector2 p = transform.parent.GetChild(1).GetComponent<Camera>().ScreenToWorldPoint(arrow);
-            transform.GetChild(0).transform.position = p;
-        }
-        else
-        {
-            transform.GetChild(0).gameObject.SetActive(false);
         }
     }
 }
